2b12209eedb5cc9d42d7b07d6dd9f079e217aa8d
rueiwang <want2813@gmail.com>
2023-05-09 14:31:19 +0800

    [feat] adjust router for future auth

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,19 +1,18 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
 import Login from "@/pages/Login";
+import ProtectedLayout from "@/pages/ProtectedLayout";
+import Home from "@/pages/Home";
 
-const router = createBrowserRouter([
-    {
-      path: "/login",
-      element: <Login />
-    },
-    {
-        path: "/home",
-        element: <div>HOME</div>
-    },
-    {
-        path: "/profile",
-        element: <div>Profile</div>
-    },
-  ]);
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+    <Route path="/login" element={<Login />} />
+    <Route path="/" element={<ProtectedLayout />}>
+      <Route path="home" element={<Home />} />
+      <Route path="profile" element={<>Profile</>} />
+    </Route>
+  </>
+  )
+);
 
-export default router
+export default router;
